124fdc362efb5f9c61174c01e046255b1d318b1f
hernanofx <hernandorra@gmail.com>
2025-07-16 17:35:17 -0300

    cors mas permisivos

--- backend/src/index.js
+++ backend/src/index.js
@@ -42,7 +42,11 @@ export const db = isPostgres
       useNullAsDefault: true
     });
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Crear tablas si no existen
